49875f7e3932d85ce5d9f12b2820a8934f83fcfd
Tim <zsa2056197@hotmail.com>
2023-08-31 17:17:28 -0600

    update

--- 230825_labjack/labjack_monitor.py
+++ 230825_labjack/labjack_monitor.py
@@ -2,6 +2,7 @@ from labjack import ljm
 import logging
 import os
 from time_util import *
+from email_test import email_warning
 
 
 def setup_logging(log_fileName):
@@ -48,47 +49,6 @@ def open_csv(fileDateStr):
     f = open(filePath, "a")
 
     return f
-
-
-def email_warning(temperature: float):
-    import smtplib
-    from email.mime.multipart import MIMEMultipart
-    from email.mime.text import MIMEText
-
-    # Email configuration
-    sender_email = "quantumdevicesgroup@gmail.com"
-    receiver_email = "zsa2056197@hotmail.com"
-    subject = "QDG-Lab Temperature Warning: {:.1f} C".format(temperature)
-    _, curTimeStr, _ = get_time_date()
-    message = "{:s} - Temperature Warning: {:.1f} C".format(curTimeStr, temperature)
-
-    # Create the email
-    msg = MIMEMultipart()
-    msg["From"] = sender_email
-    msg["To"] = receiver_email
-    msg["Subject"] = subject
-    msg.attach(MIMEText(message, "plain"))
-
-    # Connect to the SMTP server
-    smtp_server = "smtp.gmail.com"  # Change this to your SMTP server
-    smtp_port = 587  # Change this to the appropriate port
-    smtp_username = "quantumdevicesgroup@gmail.com"
-    smtp_password = "qdg373!!"
-
-    try:
-        server = smtplib.SMTP(smtp_server, smtp_port)
-        server.starttls()
-        server.login(smtp_username, smtp_password)
-
-        # Send the email
-        server.sendmail(sender_email, receiver_email, msg.as_string())
-        print("Email sent successfully!")
-
-    except Exception as e:
-        print(f"An error occurred: {str(e)}")
-
-    finally:
-        server.quit()  # type: ignore
 
 
 # >>> paramters <<<
