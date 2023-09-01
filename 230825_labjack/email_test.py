14326a74cf7ce72f5452c1de60cf51fd41d539c9
Tim <zsa2056197@hotmail.com>
2023-09-01 12:18:05 -0600

    update

--- 230825_labjack/email_test.py
+++ 230825_labjack/email_test.py
@@ -1,26 +1,49 @@
 from time_util import *
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from typing import List
+
+EMAIL_SELF = "quantumdevicesgroup@outlook.com"
+EMAIL_SLACK = "qdglab-monitor-aaaakuknki5idmca7uehje3bpq@quantumdevicesgroup.slack.com"
+EMAIL_GROUP = " quantumdevices@lists.berkeley.edu"
+# emergency_email_list = [EMAIL_SELF, EMAIL_SLACK, EMAIL_GROUP]
+# notice_email_list = [EMAIL_SELF, EMAIL_SLACK]
+emergency_email_list = [EMAIL_SELF]
+notice_email_list = [EMAIL_SELF]
 
 
-def email_warning(temperature: float, img_fileName=None):
-    import smtplib
-    from email.mime.multipart import MIMEMultipart
-    from email.mime.text import MIMEText
-    from email.mime.image import MIMEImage
-
+def temp_warning(temperature: float, img_fileName=None):
     # Email configuration
-    sender_email = "quantumdevicesgroup@outlook.com"
-    receiver_email_list = [
-        "quantumdevicesgroup@outlook.com",
-        "qdglab-monitor-aaaakuknki5idmca7uehje3bpq@quantumdevicesgroup.slack.com",
-    ]
-    subject = "QDG-Lab Temperature Warning: {:.1f} C".format(temperature)
     _, curTimeStr, _ = get_time_date()
+    subject = "[TEST] QDG-Lab temperature Warning: {:.1f} C".format(temperature)
     message = (
-        "{:s} - Cooling water temperature Warning: {:.1f} C".format(
+        "{:s} - Cooling water temperature Warning: {:.1f} C\n".format(
             curTimeStr, temperature
         )
-        + "\n This is an email for testing"
+        + "This is an email for testing"
     )
+    send_email(
+        emergency_email_list, subject=subject, msgstr=message, img_fileName=img_fileName
+    )
+
+
+def schedule_report(dateStr: str, img_fileName=None):
+    subject = "[TEST] QDG-Lab temperature schedule report: {:s}".format(dateStr)
+    message = (
+        "{:s} - Cooling water temperature schedule report:\n".format(dateStr)
+        + "This is an email for testing"
+    )
+    send_email(
+        notice_email_list, subject=subject, msgstr=message, img_fileName=img_fileName
+    )
+
+
+def send_email(receiver_list: List, subject: str, msgstr: str, img_fileName=None):
+    # Email configuration
+    sender_email = "quantumdevicesgroup@outlook.com"
+
     # Connect to the SMTP server
     smtp_server = "smtp.office365.com"  # Change this to your SMTP server
     smtp_port = 587  # Change this to the appropriate port
@@ -28,12 +51,12 @@ def email_warning(temperature: float, img_fileName=None):
     smtp_password = "qdg373!!"
 
     # Create the email
-    for receiver_email in receiver_email_list:
+    for receiver_email in receiver_list:
         msg = MIMEMultipart()
         msg["From"] = sender_email
         msg["To"] = receiver_email
         msg["Subject"] = subject
-        msg.attach(MIMEText(message, "plain"))
+        msg.attach(MIMEText(msgstr, "plain"))
         # add picture as attachment
         if img_fileName is not None:
             with open(img_fileName, "rb") as fp:
@@ -57,4 +80,5 @@ def email_warning(temperature: float, img_fileName=None):
 
 
 if __name__ == "__main__":
-    email_warning(100)
+    temp_warning(25.0)
+    schedule_report("2023-09-1")
--- 230825_labjack/load_data.py
+++ 230825_labjack/load_data.py
@@ -3,21 +3,25 @@ import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
+from typing import Union
+import logging
 
 
-def load_data(fileDateStr):
+def load_data(fileDateStr, span: str = "1D"):
     # load data from csv file
-    # before curtime, up to 1 hour
-    # return a list of data
-
     data = pd.read_csv(fileDateStr + ".csv", parse_dates=["Timestamp"])
     # Convert Temperature (C) to Fahrenheit for plotting (optional)
     # data["Temperature (F)"] = (data["Temperature (C)"] * 9 / 5) + 32
-    return data["Timestamp"], data["Temperature (C)"]
+    timedata = data["Timestamp"]
+    tempdata = data["Temperature (C)"]
+    if span == "1D":
+        return timedata, tempdata
+    elif span == "1H":
+        return timedata.tail(6 * 60), tempdata.tail(6 * 60)
 
 
-def plot_data(fileDateStr):
-    timedata, tempdata = load_data(fileDateStr)
+def plot_data(fileDateStr, span: str = "1D"):
+    timedata, tempdata = load_data(fileDateStr, span=span)  # type: ignore
 
     plt.figure(figsize=(10, 6))
     plt.plot(
@@ -35,11 +39,22 @@ def plot_data(fileDateStr):
     plt.grid(True)
     plt.xticks(rotation=45)
     plt.tight_layout()
-
-    # Show the plot
-    plt.show()
+    img_fileName = "{:s}_{:s}_temperature.png".format(fileDateStr, span)
+    logging.info("Saving image to: " + img_fileName)
+    try:
+        plt.savefig(
+            img_fileName,
+            bbox_inches="tight",
+            dpi=300,
+        )
+        logging.info("Image saved")
+    except Exception as e:
+        logging.error("Error: " + str(e))
+    #
+    return img_fileName
 
 
 if __name__ == "__main__":
     _, _, fileDateStr = get_time_date()
-    plot_data(fileDateStr)
+    img_fileName = plot_data(fileDateStr, span="1H")
+    print(img_fileName)
