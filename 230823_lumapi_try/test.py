bdb8929b9341575f5f52bc15150de503852b1a8f
Tim <zsa2056197@hotmail.com>
2023-09-03 19:23:53 -0600

    update

--- 230823_lumapi_try/test.py
+++ 230823_lumapi_try/test.py
@@ -1,4 +1,5 @@
 from lumapi_optimize import add_lumerical_path
+
 add_lumerical_path()
 import lumapi
 import numpy as np
@@ -6,13 +7,6 @@ import numpy as np
 
 with lumapi.FDTD() as fdtd:
     fdtd.addstructuregroup(name="grating")
-    fdtd.addstructuregroup(name="grating1")
-    fdtd.addstructuregroup(name="grating2")
-
-
-    fdtd.addrect()
-    fdtd.addtogroup("grating")
-    fdtd.select("grating3")
-    fdtd.delete()
-    while 1:
+    fdtd.setnamed("grating", "scripts", "fk\n\shit\n")
+    while True:
         pass
