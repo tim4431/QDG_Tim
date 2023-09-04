91157a0999ab97fc7538adcafc1a08ba2944ec9d
Tim <zsa2056197@hotmail.com>
2023-09-04 12:04:59 -0700

    update

--- 230823_lumapi_try/work_loader.py
+++ 230823_lumapi_try/work_loader.py
@@ -187,6 +187,20 @@ if __name__ == "__main__":
         "SOURCE_typ": "gaussian_released",
         "start_radius": 12e-6,
     }
+    work_4e25_crop_2 = {
+        "lambda_0": 1.326e-6,
+        "FWHM": 40e-9,
+        "alpha": 0.00,
+        "penalty": [[0.01, 10e-9], [0.02, 100e-9]],
+        "N": 9,
+        "NL": 2,
+        "NH": 2,
+        "maxiter": 101,
+        "MIN_FEATURE_SIZE": 40e-9,
+        "FOM_typ": "square",
+        "SOURCE_typ": "gaussian_released",
+        "start_radius": 12e-6,
+    }
 
     work_4e25_inverse = {
         "lambda_0": 1.326e-6,
@@ -210,8 +224,8 @@ if __name__ == "__main__":  # type: ignore
     # works = [work1, work2, work3]
     # works = [work9, work10, work11, work11_5, work12, work13, work14, work15, work16]
     # works = [work9,work10,work11,work12,work13,work14]
-    works = [work_4e25]
-    works = [work_4e25_inverse]
+    works = [work_4e25_crop_2]
+    # works = [work_4e25_inverse]
     work_loader(works, prefix="4e25_crop_range=2")
-    work_loader(works, prefix="4e25_square_bw100_inverse")
+    # work_loader(works, prefix="4e25_square_bw100_inverse")
     # work_loader(works_test)
