a7985ab3f336a16724160d66320648abda63c7b8
Tim <zsa2056197@hotmail.com>
2023-09-03 19:27:29 -0600

    update

--- 230823_lumapi_try/test.py
+++ 230823_lumapi_try/test.py
@@ -3,10 +3,18 @@ from lumapi_optimize import add_lumerical_path
 add_lumerical_path()
 import lumapi
 import numpy as np
+import os
+
+
+def load_script(script_name):
+    cwd = os.getcwd()
+    with open(os.path.join(cwd, "script", script_name), "r") as f:
+        script = f.read()
+        return script
 
 
 with lumapi.FDTD() as fdtd:
     fdtd.addstructuregroup(name="grating")
-    fdtd.setnamed("grating", "scripts", "fk\n\shit\n")
+    fdtd.setnamed("grating", "script", load_script("subw_grating_concentric.lsf"))
     while True:
         pass
