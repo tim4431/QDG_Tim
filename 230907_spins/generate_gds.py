39151d1ebba60133e091e4c96429c61a869066be
Tim <zsa2056197@hotmail.com>
2023-09-11 16:38:16 -0600

    update

--- 230907_spins/generate_gds.py
+++ 230907_spins/generate_gds.py
@@ -6,17 +6,20 @@ from lib.grating.load_grating_data import load_grating_data
 from lib.grating.grating_concentric import grating_concentric_arc
 
 
-def generate_gds(dataDirname):
+def generate_gds(dataDirname, suffix=""):
     grating = load_grating_data(
         os.path.join(".", dataDirname, "output"), grating_len=10000
     )
+    grating = [g * 1326 / 1315 for g in grating]
     c = grating_concentric_arc(
         taper_angle=24, grating_angle=24, start_radius=12.0, grating=grating
     )
     c.show()
-    c.write_gds(os.path.join(".", dataDirname, dataDirname + "_arc.gds"))
+    c.write_gds(
+        os.path.join(".", dataDirname, dataDirname + "_arc_{:s}.gds".format(suffix))
+    )
 
 
 if __name__ == "__main__":
     dataDirname = "3_grating_straight_w1326_released_1"
-    generate_gds(dataDirname)
+    generate_gds(dataDirname, suffix="expand")
