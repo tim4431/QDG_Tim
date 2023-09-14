f24a6bbc1779799777cea7cd259b107a190b3c7f
Tim <zsa2056197@hotmail.com>
2023-09-14 12:48:30 -0600

    update

---
+++ 230823_lumapi_try/grating_sweep/grating_sweep_compare_transmission.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+
+sys.path.append("../..")
+from lib.gaussian.gaussian_fit_1d import arb_fit_1d
+
+if __name__ == "__main__":
+    fig, ax = plt.subplots(figsize=(10, 6))
+    uuidList = ["e199", "793c", "e86b", "15a5", "d1fd", "6ade", "674e"]
+    lambdaList = [1200, 1240, 1280, 1320, 1360, 1400, 1440]
+    for uuid, lambda_0 in zip(uuidList, lambdaList):
+        dataName = "{:s}_{:.1f}_bw=20.0_transmission.txt".format(uuid, lambda_0)
+        data = np.loadtxt(dataName)
+        arb_fit_1d(ax, data[:, 0] * 1e3, data[:, 1], "{:.1f}".format(lambda_0))
+    plt.legend()
+    plt.savefig("grating_sweep_compare_transmission.png", dpi=200, bbox_inches="tight")
+    plt.show()
