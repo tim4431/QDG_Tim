2114be25c4fd04d72363cb4c1d992d018e8ab902
Tim <zsa2056197@hotmail.com>
2023-09-15 18:45:32 -0600

    update

--- 230825_labjack/const_var.py
+++ 230825_labjack/const_var.py
@@ -1,5 +1,5 @@
 pinName = "AIN2"
-RECORD_INTERVAL_S = 2  # in s
+RECORD_INTERVAL_S = 20  # in s
 ALERT_INTERVAL_S = 60 * 10  # in s
 TEMPERATURE_THRESHOLD = 16  # in C
 record_interval = RECORD_INTERVAL_S * 1000  # in ms
