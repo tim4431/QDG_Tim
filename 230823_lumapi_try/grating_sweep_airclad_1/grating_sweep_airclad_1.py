4ee510cef96a3a6eb9be568c200d78b22ac9029c
Tim <zsa2056197@hotmail.com>
2023-09-18 11:26:19 -0600

    update

--- 230823_lumapi_try/grating_sweep_airclad_1/grating_sweep_airclad_1.py
+++ 230823_lumapi_try/grating_sweep_airclad_1/grating_sweep_airclad_1.py
@@ -72,12 +72,12 @@ if __name__ == "__main__":
     def f1(x, A, g, d, b):
         return A * (1 + g * np.cos(4 * np.pi * d / x + b))
 
-    # def f2(x, A, g, d, b, g1, d1, b1):
-    #     return (
-    #         A
-    #         * (1 + g * np.cos(4 * np.pi * d / x + b))
-    #         * (1 + g1 * np.cos(4 * np.pi * d1 / x + b1))
-    #     )
+    def f2(x, A, g, d, b, g1, d1, b1):
+        return (
+            A
+            * (1 + g * np.cos(4 * np.pi * d / x + b))
+            * (1 + g1 * np.cos(4 * np.pi * d1 / x + b1))
+        )
 
     def plot_data_f1(A, g, d, b):
         for data in dataList:
@@ -93,51 +93,55 @@ if __name__ == "__main__":
             label=r"$fit={:.2f}*(1+{:.2f}*cos(4\pi*{:.0f}/x{:+.2f}))$".format(*popt),
         )
 
-    # def plot_data_f2(A, g, d, b, g1, d1, b1):
-    #     for data in dataList:
-    #         amp, x0, _, _ = arb_fit_1d(
-    #             ax, data[:, 0] * 1e3, data[:, 1], "", label=False
-    #         )
-    #     # popt = [0.5, 0.49, 4000, -3, 0.5, 230, 1]
-    #     popt = [A, g, d, b, g1, d1, b1]
-    #     x = np.linspace(np.min(lambdaList), np.max(lambdaList), 100)
-    #     ax.plot(
-    #         x,
-    #         f2(x, *popt),
-    #         label=r"$fit={:.2f}*(1+{:.2f}*cos(4\pi*{:.0f}/x{:+.2f}))* (1+{:.0f}*cos(4\pi*{:.2f}/x{:+.2f}))$".format(
-    #             *popt
-    #         ),
-    #     )
+    def plot_data_f2(A, g, d, b, g1, d1, b1):
+        for data in dataList:
+            amp, x0, _, _ = arb_fit_1d(
+                ax, data[:, 0] * 1e3, data[:, 1], "", label=False
+            )
+        # popt = [0.5, 0.49, 4000, -3, 0.5, 230, 1]
+        popt = [A, g, d, b, g1, d1, b1]
+        x = np.linspace(np.min(lambdaList), np.max(lambdaList), 100)
+        ax.plot(
+            x,
+            f2(x, *popt),
+            label=r"$fit={:.2f}*(1+{:.2f}*cos(4\pi*{:.0f}/x{:+.2f}))* (1+{:.0f}*cos(4\pi*{:.2f}/x{:+.2f}))$".format(
+                *popt
+            ),
+        )
 
     # print(ampList)
 
     from scipy.optimize import curve_fit
 
-    popt, pcov = curve_fit(
-        f1,
-        x0List,
-        ampList,
-        p0=[0.29, 0.40, 5210, -1.4],
-        bounds=(
-            [0, 0, 0, -2 * np.pi],
-            [1, 1, 6000, 2 * np.pi],
-        ),
-    )
     # popt, pcov = curve_fit(
-    #     f2,
+    #     f1,
     #     x0List,
     #     ampList,
-    #     p0=[0.42, 0.59, 3600, -1, 0.41, 130, 1.10],
+    #     p0=[0.29, 0.40, 5210, -1.4],
     #     bounds=(
-    #         [0, 0, 0, -2 * np.pi, 0, 0, -2 * np.pi],
-    #         [1, 1, 6000, 2 * np.pi, 1, 1000, 2 * np.pi],
+    #         [0, 0, 0, -2 * np.pi],
+    #         [1, 1, 6000, 2 * np.pi],
     #     ),
     # )
-    print(popt)
-    plot_data_f1(*popt)
-    # ax.set_xlabel("wavelength (nm)")
-    # ax.set_ylabel("transmission")
-    # ax.legend()
     #
-    # plt.savefig("grating_sweep_compare_transmission.png", dpi=200, bbox_inches="tight")
+    popt, pcov = curve_fit(
+        f2,
+        x0List,
+        ampList,
+        p0=[0.27, 0.37, 5161, -0.97, 0.19, 560, 1.58],
+        bounds=(
+            [0, 0, 0, -2 * np.pi, 0, 0, -2 * np.pi],
+            [1, 1, 6000, 2 * np.pi, 1, 1000, 2 * np.pi],
+        ),
+    )
+    #
+    print(popt)
+    popt = [0.27, 0.37, 5161, -0.97, 0.19, 560, 1.58]
+    # plot_data_f1(*popt)
+    plot_data_f2(*popt)
+    ax.set_xlabel("wavelength (nm)")
+    ax.set_ylabel("transmission")
+    ax.legend()
+    #
+    plt.savefig("grating_sweep_compare_airclad_1.png", dpi=200, bbox_inches="tight")
     plt.show()
