a0fe2e560b713816ade3122fe0241c78719788c2
Tim <zsa2056197@hotmail.com>
2023-09-22 17:13:29 -0700

    hello from QDG5

--- 230825_labjack/const_var.py
+++ 230825_labjack/const_var.py
@@ -1,5 +1,5 @@
 pinName = "AIN2"
 RECORD_INTERVAL_S = 20  # in s
-ALERT_INTERVAL_S = 60 * 10  # in s
+ALERT_INTERVAL_S = 60 * 30  # in s
 TEMPERATURE_THRESHOLD = 16  # in C
 record_interval = RECORD_INTERVAL_S * 1000  # in ms
