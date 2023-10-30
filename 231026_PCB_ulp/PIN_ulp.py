b7456ff19b4cf8a4634f64d0be4a994971063514
Tim <zsa2056197@hotmail.com>
2023-10-30 12:30:15 -0600

    update

--- 231026_PCB_ulp/PIN_ulp.py
+++ 231026_PCB_ulp/PIN_ulp.py
@@ -13,18 +13,18 @@ def move_comp(compName: str, x, y):
     intruction_str_List.append(move_comp_str(compName, x, y))
 
 
-compNameDCU = ["U$11", "U$10", "U$9", "U$8"]
-compNameDCB = ["U$5", "U$4", "U$3", "U$2"]
+compNameDCU = ["U$14", "U$7", "U$2", "U$3", "U$4", "U$5"]
+compNameDCB = ["U$16", "U$15", "U$8", "U$9", "U$10", "U$11"]
 compNameMW = ["U$13", "U$1", "U$12", "U$6"]
 
 H1Board = 1845
 HMID = H1Board / 2
-move_comp("J2", 3030, 1885)
-move_comp("J1", 3030, -40)
-DIS_SMA_1 = 340
-DIS_SMA_2 = 910
-X_SMA_1 = 2790
-X_SMA_2 = 3140
+move_comp("J2", 3400, H1Board + 150)
+move_comp("J1", 3400, -150)
+DIS_SMA_1 = 380
+DIS_SMA_2 = 970
+X_SMA_1 = 2910
+X_SMA_2 = 3360
 move_comp("J4", X_SMA_1, HMID + DIS_SMA_1 / 2)
 move_comp("J3", X_SMA_1, HMID - DIS_SMA_1 / 2)
 move_comp("J6", X_SMA_2, HMID + DIS_SMA_2 / 2)
