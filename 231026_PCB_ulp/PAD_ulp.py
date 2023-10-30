932598f40aef2c02227186ce8a51388b6119cbbe
Tim <zsa2056197@hotmail.com>
2023-10-30 13:41:18 -0600

    update

--- 231026_PCB_ulp/PAD_ulp.py
+++ 231026_PCB_ulp/PAD_ulp.py
@@ -38,6 +38,7 @@ def _merge_y(ys, MIN_DIS):
 
 
 def get_via_ys(traces, x, w_via):
+    MIN_VIA_DIS = 30
     ys = []
     for trace in traces:
         trace_y = get_trace_y(trace, x)
@@ -48,7 +49,6 @@ def get_via_ys(traces, x, w_via):
             ys.append(trace_y + trace_dis_y)
             ys.append(trace_y - trace_dis_y)
     ys = sorted(ys)
-    MIN_VIA_DIS = 30
     return _merge_y(ys, MIN_VIA_DIS)
 
 
@@ -59,11 +59,40 @@ traces = [
     (448, 772, 3360, 372),
 ]
 x = np.linspace(448, 3360, 25)
+points = []
 for trace in traces:
     plt.plot([trace[0], trace[2]], [trace[1], trace[3]], c="b")
 for x_ in x:
     ys = get_via_ys(traces, x_, 100)
-    plt.scatter([x_] * len(ys), ys, c="r")
+    # plt.scatter([x_] * len(ys), ys, c="r")
+    points += [(x_, y) for y in ys]
 
+
+#
+def judge_if_close(points, x, y):
+    MIN_VIA_DIS_LARGE = 100
+    for x_, y_ in points:
+        if ((x_ - x) ** 2 + (y_ - y) ** 2) < MIN_VIA_DIS_LARGE**2:
+            return True
+    return False
+
+
+# add another grid of vias, but if any via is too close to the previous one, remove it
+XMAX = 3582
+YMAX = 2160
+YMIN = -315
+VIA_BG_DIS = 120
+grid_x, grid_y = np.meshgrid(
+    np.linspace(0, XMAX, round(XMAX / VIA_BG_DIS)),
+    np.linspace(YMIN, YMAX, round((YMAX - YMIN) / VIA_BG_DIS)),
+)
+grid_points = [(x_, y_) for x_, y_ in zip(grid_x.flatten(), grid_y.flatten())]
+grid_points_keep = [
+    (x_, y_) for x_, y_ in grid_points if not judge_if_close(points, x_, y_)
+]
+#
+points.extend(grid_points_keep)
+plt.scatter([x for x, y in points], [y for x, y in points], c="r")
 
 plt.show()
+print(" ".join([via_gnd(x, y) for x, y in points]))
