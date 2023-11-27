ee11b4594e476a7956c9520cebc50e2465391b24
Tim <zsa2056197@hotmail.com>
2023-11-27 10:46:24 -0800

    update

---
+++ 231124_Instatune/insta_LUT.py
@@ -0,0 +1,58 @@
+import serial
+
+
+def create_command_packet(register_address, write_value):
+    byte0 = 1 << 31 | register_address << 25
+    byte1 = (write_value >> 8) & 0xFF  # MSB
+    byte2 = write_value & 0xFF  # LSB
+    byte3 = 0  # Checksum placeholder
+    return (
+        byte0.to_bytes(1, "big")
+        + byte1.to_bytes(1, "big")
+        + byte2.to_bytes(1, "big")
+        + byte3.to_bytes(1, "big")
+    )
+
+
+def write_to_register(ser, register_address, write_value):
+    command_packet = create_command_packet(register_address, write_value)
+    ser.write(command_packet)
+    # Optionally, read the response here
+
+
+def write_LUT(ser, lut):
+    # Set LUT Start Index and Length
+    lut_start_index = 0  # Example starting index
+    lut_length = len(lut)  # Calculate length of the LUT
+    write_to_register(ser, 0x20, lut_start_index)
+    write_to_register(ser, 0x21, lut_length)
+
+    # Write LUT data
+    for row in lut:
+        front_mirror, back_mirror, phase, soa = row
+
+        # Prepare and write for Front Mirror
+        write_to_register(ser, 0x1E, 0)  # 0 for Front Mirror
+        write_to_register(ser, 0x1F, front_mirror)
+
+        # Prepare and write for Back Mirror
+        write_to_register(ser, 0x1E, 1)  # 1 for Back Mirror
+        write_to_register(ser, 0x1F, back_mirror)
+
+        # Prepare and write for Phase
+        write_to_register(ser, 0x1E, 2)  # 2 for Phase
+        write_to_register(ser, 0x1F, phase)
+
+        # Prepare and write for SOA
+        write_to_register(ser, 0x1E, 3)  # 3 for SOA
+        write_to_register(ser, 0x1F, soa)
+
+
+if __name__ == "__main__":
+    # Open serial communication
+    ser = serial.Serial("COM14", 9600)  # Replace 'COM_PORT' with the actual COM port
+    from read_LUT import read_LUT
+
+    lut = read_LUT([1325, 1326, 1327])
+    write_LUT(ser, lut)
+    ser.close()
---
+++ 231124_Instatune/read_LUT.py
@@ -0,0 +1,46 @@
+import csv
+
+
+def read_nearest_wavelength(file_path, target_wavelength):
+    with open(file_path, "r") as file:
+        reader = csv.DictReader(file)
+        nearest_row = {}
+        min_difference = float("inf")
+
+        for row in reader:
+            wavelength = float(row["WL Target"])
+            difference = abs(wavelength - target_wavelength)
+            if difference < min_difference:
+                min_difference = difference
+                nearest_row = row
+
+        # return nearest_row
+        # (Front Mirror, Back Mirror, Phase, SOA)
+        FM_DAC = float(nearest_row["FM DAC"])
+        BM_DAC = float(nearest_row["BM DAC"])
+        PH_DAC = float(nearest_row["PH DAC"])
+        SOA_DAC = float(nearest_row["SOA DAC"])
+        WL_Target = float(nearest_row["WL Target"])
+        if abs(WL_Target - target_wavelength) > 0.1:
+            raise ValueError(
+                "Nearest wavelength {:.4f} is too far from target wavelength {:.4f}"
+            )
+        else:
+            print("Find nearest wavelength {:.4f}".format(WL_Target))
+        return (FM_DAC, BM_DAC, PH_DAC, SOA_DAC)
+
+
+def read_LUT(wavelength_list):
+    lut = []
+    file_path = "/home/tim/Code/QDG_Tim/231124_Instatune/DJ133_LUT_0v0.csv"
+    for wavelength in wavelength_list:
+        nearest_row = read_nearest_wavelength(file_path, wavelength)
+        # print(nearest_row)
+        lut.append(nearest_row)
+    return lut
+
+
+if __name__ == "__main__":
+    wavelength_list = [1325, 1326, 1327]
+    lut = read_LUT(wavelength_list)
+    print(lut)
