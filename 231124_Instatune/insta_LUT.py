6704edc004f8118a8ea51db654b327ccf5d1e588
Tim <zsa2056197@hotmail.com>
2023-11-27 17:30:06 -0800

    update

--- 231124_Instatune/insta_LUT.py
+++ 231124_Instatune/insta_LUT.py
@@ -18,7 +18,7 @@ def write_command_packet(register_address, write_value):
     msb = (write_value >> 8) & 0xFF  # Most Significant Byte
     lsb = write_value & 0xFF  # Least Significant Byte
     checksum = 0  # Placeholder for checksum
-    print(byte0, msb, lsb, checksum)
+    # print(byte0, msb, lsb, checksum)
     # Format string 'BBBB' for big-endian, 1 byte each for Byte 0, Byte 1, Byte 2, and Byte 3 (Checksum)
     return struct.pack(">BBBB", byte0, msb, lsb, checksum)
 
@@ -26,7 +26,7 @@ def write_command_packet(register_address, write_value):
 def write_to_register(ser, register_address, write_value):
     command_packet = write_command_packet(register_address, write_value)
     print(command_packet)
-    # ser.write(command_packet)
+    ser.write(command_packet)
     # Optionally, read the response here
     response = read_response(ser)
     if response:
@@ -36,6 +36,8 @@ def write_to_register(ser, register_address, write_value):
                 register_address, msb, lsb, checksum, interpret_status(status)
             )
         )
+    else:
+        print("No response received")
 
 
 def read_response(ser):
@@ -64,40 +66,69 @@ def interpret_status(status):
     return status_messages.get(status, "Unknown status")
 
 
-def write_LUT(ser, lut):
-    # Set LUT Start Index and Length
-    lut_start_index = 0  # Example starting index
-    lut_length = len(lut)  # Calculate length of the LUT
-    write_to_register(ser, 0x20, lut_start_index)
-    write_to_register(ser, 0x21, lut_length)
+def write_DAC(ser, DAC):
+    # Set DAC Start Index and Length
+    DAC_start_index = 0  # Example starting index
+    DAC_length = len(DAC)  # Calculate length of the DAC
+    write_to_register(ser, 0x20, DAC_start_index)
+    write_to_register(ser, 0x21, DAC_length)
 
-    # Write LUT data
-    for row in lut:
-        front_mirror, back_mirror, phase, soa = row
+    # Write DAC data
+    FM_DAC = [row[0] for row in DAC]
+    BM_DAC = [row[1] for row in DAC]
+    PH_DAC = [row[2] for row in DAC]
+    SOA_DAC = [row[3] for row in DAC]
 
-        # Prepare and write for Front Mirror
-        write_to_register(ser, 0x1E, 0)  # 0 for Front Mirror
-        write_to_register(ser, 0x1F, front_mirror)
 
-        # Prepare and write for Back Mirror
-        write_to_register(ser, 0x1E, 1)  # 1 for Back Mirror
-        write_to_register(ser, 0x1F, back_mirror)
+    # Prepare and write for Front Mirror
+    write_to_register(ser, 0x1E, 0)  # 0 for Front Mirror
+    for FM in FM_DAC:
+        write_to_register(ser, 0x1F, FM)
 
-        # Prepare and write for Phase
-        write_to_register(ser, 0x1E, 2)  # 2 for Phase
-        write_to_register(ser, 0x1F, phase)
+    # Prepare and write for Back Mirror
+    write_to_register(ser, 0x1E, 1)  # 1 for Back Mirror
+    for BM in BM_DAC:
+        write_to_register(ser, 0x1F, BM)
 
-        # Prepare and write for SOA
-        write_to_register(ser, 0x1E, 3)  # 3 for SOA
-        write_to_register(ser, 0x1F, soa)
+    # Prepare and write for Phase
+    write_to_register(ser, 0x1E, 2)  # 2 for Phase
+    for PH in PH_DAC:
+        write_to_register(ser, 0x1F, PH)
 
+    # Prepare and write for SOA
+    write_to_register(ser, 0x1E, 3)  # 3 for SOA
+    for SOA in SOA_DAC:
+        write_to_register(ser, 0x1F, SOA)
+
+def write_laser_sweep(ser,sweep):
+    if sweep:
+        write_to_register(ser, 0x27, 0x0F00)
+        # write_to_register(ser, 0x27, 0x0100)
+    else:
+        write_to_register(ser, 0x27, 0x0000)
+
+def write_laser_status(ser,status):
+    """
+    status: 0 for disable, 1 for enable
+    """
+    if status:
+        write_to_register(ser, 0x10, 0x9FFF)
+    else:
+        write_to_register(ser, 0x10, 0x0000)
+
+
+def write_index(ser, index):
+    write_to_register(ser, 0x15, index)
 
 if __name__ == "__main__":
     # Open serial communication
-    # ser = serial.Serial("COM14", 9600)  # Replace 'COM_PORT' with the actual COM port
-    ser = None
-    from read_LUT import read_LUT
+    ser = serial.Serial("COM4", 9600)  # Replace 'COM_PORT' with the actual COM port
+    # ser = None
+    from read_LUT import load_DAC
 
-    lut = read_LUT([1325, 1326, 1327])
-    write_LUT(ser, lut)
+    # DAC = load_DAC([1324,1325])
+    # write_DAC(ser, DAC)
+    # write_laser_status(ser,1)
+    write_laser_sweep(ser,1)
+    # write_index(ser,2)
     ser.close()
--- 231124_Instatune/read_LUT.py
+++ 231124_Instatune/read_LUT.py
@@ -3,8 +3,10 @@ import csv
 
 import numpy as np
 
-def read_nearest_wavelength(file_path, target_wavelengths):
+def load_LUT(target_wavelengths):
+    file_path = "./DJ133_LUT_0v0.csv"
     data = np.genfromtxt(file_path, delimiter=',', skip_header=1)
+    IDX = data[:,0]
     FM_DAC = data[:,1]
     BM_DAC = data[:,2]
     PH_DAC = data[:,3]
@@ -19,18 +21,26 @@ def read_nearest_wavelength(file_path, target_wavelengths):
             raise ValueError("Nearest wavelength {:.4f} is too far from target wavelength {:.4f}".format(WL_Target[nearest_index], target_wavelength))
         else:
             print("Find nearest wavelength {:.4f}".format(nearest_row[5]))
-            val = (int(FM_DAC[nearest_index]), int(BM_DAC[nearest_index]), int(PH_DAC[nearest_index]), int(SOA_DAC[nearest_index]))
+            val = [int(IDX[nearest_index]),int(FM_DAC[nearest_index]), int(BM_DAC[nearest_index]),\
+                    int(PH_DAC[nearest_index]), int(SOA_DAC[nearest_index]),float(WL_Target[nearest_index])]
             vals.append(val)
     return vals
 
 
-def read_LUT(wavelength_list):
-    lut = []
-    file_path = "./DJ133_LUT_0v0.csv"
-    return read_nearest_wavelength(file_path, wavelength_list)
+def load_DAC(wavelength_list):
+    vals = load_LUT(wavelength_list)
+    DAC = [val[1:5] for val in vals]
+    return DAC
 
+def save_LUT(file_path, lut):
+    with open(file_path, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',')
+        writer.writerow(['IDX', 'FM_DAC', 'BM_DAC', 'PH_DAC', 'SOA_DAC', 'WL_Target'])
+        for i,val in enumerate(lut):
+            val[0] = i
+            writer.writerow(val)
 
 if __name__ == "__main__":
-    wavelength_list = [1325, 1326, 1327]
-    lut = read_LUT(wavelength_list)
-    print(lut)
+    wavelength_list = [1310, 1315,1320]
+    lut = load_LUT(wavelength_list)
+    save_LUT("./DJ133_LUT_temp.csv", lut)
